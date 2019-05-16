4ef1ee9803402e3530ac57b41b62f87d9f5ef72d
Jose Fernandez <josefd8@gmaill.com>
2019-05-16 11:50:58 +0200

    Updated CucumberBackendIT

--- src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberBackendIT.java
+++ src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberBackendIT.java
@@ -1,26 +1,19 @@
 package ${groupId}.${artifactId}.${package};
 
-import com.privalia.qa.cucumber.testng.CucumberRunner;
 import cucumber.api.CucumberOptions;
-import org.testng.annotations.Test;
 import ${groupId}.${artifactId}.utils.BaseTest;
-
 
 
 /**
  * This is an example on how to call an specific feature to be executed.
  * Glue files can be specified in the {@link CucumberOptions} annotation
  */
-@CucumberOptions(format = "json:target/CucumberBackendIT.json", features =
+@CucumberOptions(features =
         {
                 "src/test/resources/features/$package/cucumber_backend_test.feature"
         },
         glue = "classpath:${groupId}.${artifactId}.specs")
 public class CucumberBackendIT extends BaseTest {
 
-    @Test(enabled = true)
-    public void runSmokeTests() throws Exception {
-        new CucumberRunner(this.getClass()).runCukes();
-    }
 
 }
