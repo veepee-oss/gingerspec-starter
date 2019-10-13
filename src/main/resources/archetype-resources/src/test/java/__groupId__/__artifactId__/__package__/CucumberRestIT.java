8ce14598d0e703e839d9b2ce78a005e161a28655
jose.fernandez <josefd8@gmaill.com>
2019-10-13 16:08:55 +0200

    Added cucumber option to generate html reports in /target folder

--- src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberRestIT.java
+++ src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberRestIT.java
@@ -8,7 +8,10 @@ import ${groupId}.${artifactId}.utils.BaseTest;
  * This is an example on how to call an specific feature to be executed.
  * Glue files can be specified in the {@link CucumberOptions} annotation
  */
-@CucumberOptions(plugin = {"json:target/CucumberRestIT.json"}, features =
+@CucumberOptions(plugin = {
+                "json:target/CucumberRestIT.json",
+                "html:target/CucumberRestIT",
+        }, features =
         {
                 "src/test/resources/features/$package/cucumber_rest_test.feature"
         },
