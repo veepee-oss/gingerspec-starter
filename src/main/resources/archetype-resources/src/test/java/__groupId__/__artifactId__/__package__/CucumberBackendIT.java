90cbdc8b71cec380cd8a64a54267e4a67b57c869
jose.fernandez <josefd8@gmaill.com>
2020-06-06 21:26:21 +0200

    Some improvements in messages and instructions

--- src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberBackendIT.java
+++ src/main/resources/archetype-resources/src/test/java/__groupId__/__artifactId__/__package__/CucumberBackendIT.java
@@ -7,6 +7,9 @@ import ${groupId}.${artifactId}.utils.BaseTest;
 /**
  * This is an example on how to call an specific feature to be executed.
  * Glue files can be specified in the {@link CucumberOptions} annotation
+ *
+ * You can specify more than one feature file, or even a folder containing
+ * features
  */
 @CucumberOptions(plugin = {
                 "json:target/CucumberBackendIT.json",
